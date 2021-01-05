94d48ded5b9f990081e206b4fc9bbfc3cefa8cdf
Geonho Kim <gunhoflash@gmail.com>
2021-01-05 16:09:08 +0900

    Show and hide sections

--- public/js/js.js
+++ public/js/js.js
@@ -16,24 +16,67 @@ function findOne(str) {
   return document.querySelector(str)
 }
 
+function hide(str) {
+  find(str).forEach(element => {
+    element.classList.add('d-none')
+  })
+}
+
+function hideOne(str) {
+  findOne(str).classList.add('d-none')
+}
+
+function show(str) {
+  find(str).forEach(element => {
+    element.classList.remove('d-none')
+  })
+}
+
+function hideOne(str) {
+  findOne(str).classList.remove('d-none')
+}
+
 /*
 
   nav item selection
 
 */
-find('.nav-item')
-.forEach(element => {
-  element.addEventListener('click', selectNavItem)
-})
 
-function selectNavItem(e) {
-  const nav = e.target.dataset.nav
-
-  find(`[data-nav=${nav}]`)
-  .forEach(element => {
-    element.removeAttribute('selected')
+function initNavItems() {
+  find('.nav-item').forEach(element => {
+    element.addEventListener('click', clickNavItem)
+    if (element.dataset.selected === 'true') {
+      selectNavItem(element)
+    }
   })
-
-  e.target.setAttribute('selected', null)
 }
 
+function clickNavItem(e) {
+  selectNavItem(e.target)
+}
+
+function selectNavItem(navItem) {
+  const navSelector = navItem.dataset.nav
+
+  // focus off all elements
+  find(`[data-nav=${navSelector}]`).forEach(element => {
+    element.dataset.selected = 'false'
+
+    // hide the target
+    hide(element.dataset.target)
+  })
+  
+  // focus on the element
+  navItem.dataset.selected = 'true'
+  
+  // show the target
+  show(navItem.dataset.target)
+}
+
+/*
+
+  init
+
+*/
+
+initNavItems()
