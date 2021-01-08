c52797d33a1c27e44baebd48d79f087a9bcf342e
Geonho Kim <gunhoflash@gmail.com>
2021-01-09 00:01:51 +0900

    Add functions for carousel

--- public/js/js.js
+++ public/js/js.js
@@ -71,8 +71,88 @@ function selectNavItem(navItem) {
 
 /*
 
+  carousel
+
+*/
+
+function initCarousel() {
+  find('.carousel').forEach(element => {
+    const carouselId = element.getAttribute('id')
+    const items = find(`#${carouselId} .carousel-item`)
+    const carouselInfo = {
+      index: 0,
+      items: items,
+      nItems: items.length,
+      isOnTransition: false,
+    }
+
+    // add click event listener to prev button
+    findOne(`#${carouselId} .carousel-button-prev`)
+    .addEventListener('click', () => showNextCarouselItem(carouselInfo, true))
+
+    // add click event listener to next button
+    findOne(`#${carouselId} .carousel-button-next`)
+    .addEventListener('click', () => showNextCarouselItem(carouselInfo))
+
+    // show first item
+    showNextCarouselItem(carouselInfo)
+
+    // set interval event
+    setInterval(() => showNextCarouselItem(carouselInfo), 5000)
+  })
+}
+
+function showNextCarouselItem(carouselInfo, isReversed = false) {
+  const { items, nItems, isOnTransition } = carouselInfo
+  let { index } = carouselInfo
+
+  // prevent duplicated transition
+  if (isOnTransition) return
+  carouselInfo.isOnTransition = true
+
+  // set index
+  if (isReversed)
+    index--
+  else
+    index++
+  index = (index + nItems) % nItems
+  carouselInfo.index = index
+
+  // find items to transit
+  const prevItem = items[(index - 1 + nItems) % nItems]
+  const nowItem = items[index]
+  const nextItem = items[(index + 1) % nItems]
+
+  // remove all prev/active/next classes
+  items.forEach(item => {
+    item.classList.remove('prev')
+    item.classList.remove('active')
+    item.classList.remove('next')
+  })
+
+  // add prev/active/next class
+  prevItem.classList.add('prev')
+  nowItem.classList.add('active')
+  nextItem.classList.add('next')
+
+  // add reverse class
+  if (isReversed) {
+    prevItem.classList.add('reverse')
+    nowItem.classList.add('reverse')
+    nextItem.classList.add('reverse')
+  }
+
+  // end of transition
+  setTimeout(() => {
+    carouselInfo.isOnTransition = false
+  }, 300)
+}
+
+/*
+
   init
 
 */
 
 initNavItems()
+initCarousel()
